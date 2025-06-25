470d3443b7a7557ae19ed3e4bffa93892e643873
louhabali <louhabali@gmail.com>
2025-06-25 12:30:46 +0100

    chacking unsolvable

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,5 +1,17 @@
-export function FillGrid (grid,randoms,size) {
-    for (let i = 1; i <= size; i++) {
+export function FillGrid(grid, randoms, size) {
+  let nums = Array.from({ length: size * size - 1 }, (_, i) => i + 1)
+  let isSolvable = false
+  let flat = []
+
+  while (!isSolvable) {
+    nums.sort(() => Math.random() - 0.5)
+    flat = [...nums, '']
+
+    if (isPuzzleSolvable(flat, size)) {
+      isSolvable = true
+    }
+  }
+  for (let i = 1; i <= size; i++) {
           let rows = []
           for (let j = 1; j <= size; j++) {
             if (i == size && j == size) {
@@ -18,4 +30,23 @@ export function FillGrid (grid,randoms,size) {
           }
           grid.push(rows)
         }
+}
+
+function isPuzzleSolvable(tiles, size) {
+  let invCount = 0
+  for (let i = 0; i < tiles.length; i++) {
+    for (let j = i + 1; j < tiles.length; j++) {
+      if (tiles[i] !== '' && tiles[j] !== '' && tiles[i] > tiles[j]) {
+        invCount++
+      }
+    }
+  }
+
+  if (size % 2 !== 0) {
+    return invCount % 2 === 0 
+  } else {
+    let blankRow = Math.floor(tiles.indexOf('') / size)
+    let rowFromBottom = size - blankRow
+    return (invCount + rowFromBottom) % 2 === 0
+  }
 }
